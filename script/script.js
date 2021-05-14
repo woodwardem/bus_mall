861c5f985050472373c8cf3e64a063685937e169
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-05-14 08:56:41 -0700

    bus mall

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+
+img1.addEventListener('click', function(){
+    console.log('I am img1')
+})
+   
+img2.addEventListener('click', function(){
+    console.log('I am img2')
+})
+
+img3.addEventListener('click', function(){
+    console.log('I am img3')
+})
+function runFunctionImage() {
+    console.log('I am in img1');
+}
